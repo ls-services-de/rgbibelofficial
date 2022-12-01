4bbae55e4cea03ad1c2e7f4d63f5f151d78bf276
ls-services-de <119621121+ls-services-de@users.noreply.github.com>
2022-12-01 19:02:25 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const form = document.querySelector("form"),
+statusTxt = form.querySelector(".button-area span");
+
+form.onsubmit = (e)=>{
+    e.preventDefault();
+    statusTxt
+
+}
+
+
+
+
+
+
