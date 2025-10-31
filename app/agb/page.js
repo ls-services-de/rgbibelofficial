5de428c3f76546cc4c54ff2aaaac53dc58e4a790
Liam Schneider <119621121+ls-services-de@users.noreply.github.com>
2025-10-31 16:15:24 +0100

    Update AGB

--- app/agb/page.js
+++ app/agb/page.js
@@ -18,117 +18,102 @@ const AGB = () => {
           <h1 className="text-5xl font-bold text-[#04cefe] mb-8">ALLGEMEINE GESCHÄFTSBEDINGUNGEN (AGB)</h1>
 
           {/* AGB Content */}
-          <div className="bg-white bg-opacity-10 p-6 rounded-lg shadow-lg w-full max-w-lg text-left">
-            <p className="text-lg font-semibold">1) Geltungsbereich</p>
+          <div className="bg-white bg-opacity-10 p-6 rounded-lg shadow-lg w-full max-w-2xl text-left">
+            {/* Geltungsbereich */}
+            <p className="text-lg font-semibold">Geltungsbereich</p>
             <p>
-              1.1 Diese Allgemeinen Geschäftsbedingungen (AGB) der RGBibel UG (nachfolgend „Verkäufer") gelten für alle
-              Verträge zur Lieferung von Waren, die ein Kunde mit dem Verkäufer über den Online-Shop des Verkäufers
-              abschließt. Etwaige abweichende AGB des Kunden werden nicht berücksichtigt. Es gelten ausschließlich diese
-              AGB.
+              Die nachfolgenden Allgemeinen Geschäftsbedingungen (AGB) von RGBibelOfficial (nachfolgend „Verkäufer“ oder „R.“) 
+              gelten für alle Verträge über den Kauf von Waren, die ein Kunde über den Online-Shop des Verkäufers abschließt. 
+              Abweichende Bedingungen des Kunden erkennen wir nicht an. Diese AGB gelten auch für digitale Produkte oder Lizenzschlüssel, 
+              die zur Nutzung der bestellten Ware erforderlich sind.
             </p>
             <p>
-              1.2 Diese AGB gelten ebenfalls für Verträge zur Bereitstellung von Lizenzschlüsseln oder digitalen
-              Produkten, die für die Nutzung der Ware erforderlich sind.
-            </p>
-            <p>
-              1.3 Verbraucher im Sinne dieser AGB ist jede natürliche Person, die zu Zwecken handelt, die überwiegend
-              nicht ihrer gewerblichen oder selbständigen beruflichen Tätigkeit zugerechnet werden können.
-            </p>
-            <p>
-              1.4 Unternehmer im Sinne dieser AGB ist eine natürliche oder juristische Person oder eine rechtsfähige
-              Personengesellschaft, die bei Abschluss eines Rechtsgeschäfts in Ausübung ihrer gewerblichen oder
-              selbständigen beruflichen Tätigkeit handelt.
+              Verbraucher im Sinne dieser AGB sind natürliche Personen, die ein Rechtsgeschäft zu Zwecken abschließen, 
+              die überwiegend weder ihrer gewerblichen noch ihrer selbständigen beruflichen Tätigkeit zugerechnet werden können. 
+              Unternehmer sind natürliche oder juristische Personen oder rechtsfähige Personengesellschaften, die bei Abschluss eines 
+              Rechtsgeschäfts in Ausübung ihrer gewerblichen oder selbständigen beruflichen Tätigkeit handeln.
             </p>
 
-            <p className="text-lg font-semibold mt-4">2) Vertragsschluss</p>
+            <div className="border-t border-gray-500 my-6"></div>
+
+            {/* Vertragsschluss */}
+            <p className="text-lg font-semibold">Vertragsschluss</p>
             <p>
-              2.1 Die Produktbeschreibung im Online-Shop ist kein Angebot, sondern eine Aufforderung zur Abgabe eines
-              Angebots durch den Kunden.
-            </p>
-            <p>
-              2.2 Der Kunde gibt sein verbindliches Angebot durch das Absenden seiner Bestellung ab. Der Vertrag kommt
-              zustande, wenn der Verkäufer das Angebot des Kunden durch eine Auftragsbestätigung oder durch Lieferung
-              der Ware annimmt.
-            </p>
-            <p>
-              2.3 Das Angebot des Kunden wird vom Verkäufer innerhalb von fünf Tagen angenommen, entweder durch eine
-              Bestätigung oder durch Lieferung. Ohne diese Annahme kommt kein Vertrag zustande.
-            </p>
-            <p>
-              2.4 Bei der Zahlungsmethode PayPal erfolgt die Annahme des Angebots des Kunden mit der Bestätigung der
-              Zahlung.
+              Die Produktdarstellung im Online-Shop stellt kein bindendes Angebot dar, sondern eine Aufforderung zur Abgabe einer Bestellung 
+              durch den Kunden. Durch das Absenden der Bestellung gibt der Kunde ein verbindliches Angebot ab. Der Vertrag kommt zustande, 
+              wenn der Verkäufer dieses Angebot durch eine Auftragsbestätigung per E-Mail oder durch Lieferung der Ware annimmt. 
+              Bei Zahlungen über PayPal erfolgt die Annahme des Angebots mit der Bestätigung der Zahlung. Ohne diese Annahme kommt kein Vertrag zustande.
             </p>
 
-            <p className="text-lg font-semibold mt-4">3) Widerrufsrecht</p>
-            <p>
-              3.1 Verbraucher haben grundsätzlich ein Widerrufsrecht gemäß der Widerrufsbelehrung, die dem Kunden vor
-              Abschluss der Bestellung zur Verfügung gestellt wird. Missbrauch des Widerrufsrechts wird nicht toleriert.
-            </p>
-            <p>3.2 Widerrufe, die außerhalb der festgelegten Fristen eingehen, werden nicht berücksichtigt.</p>
+            <div className="border-t border-gray-500 my-6"></div>
 
-            <p className="text-lg font-semibold mt-4">4) Preise und Zahlungsbedingungen</p>
+            {/* Preise und Zahlung */}
+            <p className="text-lg font-semibold">Preise und Zahlungsbedingungen</p>
             <p>
-              4.1 Alle angegebenen Preise sind Endpreise, die die gesetzliche Mehrwertsteuer beinhalten. Eventuelle
-              Versandkosten werden zusätzlich berechnet und gesondert ausgewiesen.
-            </p>
-            <p>
-              4.2 Die Zahlung ist unmittelbar nach Bestellabschluss fällig. Erfolgt keine Zahlung innerhalb von sieben
-              Tagen, wird die Bestellung ohne weitere Ankündigung storniert.
-            </p>
-            <p>
-              4.3 Bei Wahl der Zahlungsmethode PayPal akzeptiert der Verkäufer das Angebot des Kunden mit der
-              Bestätigung der Zahlung. Es sind keine weiteren Bedingungen erforderlich.
+              Alle Preise im Shop verstehen sich als Endpreise nach der Kleinunternehmerregelung gemäß § 19 UStG, es wird keine Mehrwertsteuer ausgewiesen. 
+              Etwaige Versandkosten werden separat berechnet und gesondert angezeigt. Die Zahlung ist unmittelbar nach Bestellabschluss fällig. 
+              Erfolgt keine Zahlung innerhalb von sieben Tagen, behält sich der Verkäufer vor, die Bestellung zu stornieren.
             </p>
 
-            <p className="text-lg font-semibold mt-4">5) Lieferung</p>
+            <div className="border-t border-gray-500 my-6"></div>
+
+            {/* Lieferung */}
+            <p className="text-lg font-semibold">Lieferung</p>
             <p>
-              5.1 Die Lieferung erfolgt nur an die vom Kunden angegebene Adresse. Bei Änderungen der Lieferadresse nach
-              Bestellabschluss werden zusätzliche Versandkosten berechnet.
-            </p>
-            <p>
-              5.2 Teillieferungen können ohne Rücksprache erfolgen. Sollte eine Teillieferung notwendig sein, entstehen
-              dem Kunden keine zusätzlichen Kosten.
-            </p>
-            <p>
-              5.3 Wir liefern ausschließlich innerhalb Deutschlands und der EU. Für Lieferungen außerhalb dieses Gebiets
-              muss eine gesonderte Vereinbarung getroffen werden.
+              Die Lieferung erfolgt ausschließlich an die vom Kunden angegebene Lieferadresse. Änderungen der Lieferadresse nach Bestellabschluss 
+              können zu zusätzlichen Versandkosten führen. Teillieferungen sind zulässig und können ohne gesonderte Ankündigung erfolgen; 
+              dem Kunden entstehen daraus keine zusätzlichen Kosten. Lieferungen erfolgen grundsätzlich innerhalb Deutschlands. 
+              Lieferungen in die übrige EU oder in andere Länder bedürfen einer gesonderten Vereinbarung.
             </p>
 
-            <p className="text-lg font-semibold mt-4">6) Gewährleistung und Haftung</p>
+            <div className="border-t border-gray-500 my-6"></div>
+
+            {/* Gewährleistung und Garantie */}
+            <p className="text-lg font-semibold">Gewährleistung und Garantie</p>
             <p>
-              6.1 Es gilt das gesetzliche Mängelhaftungsrecht. Alle Mängel müssen innerhalb der gesetzlichen Fristen
-              gemeldet werden, um Gewährleistungsansprüche geltend zu machen.
-            </p>
-            <p>
-              6.2 Garantieansprüche werden ausgeschlossen, wenn der Mangel durch unsachgemäße Nutzung oder Veränderungen
-              der Ware entstanden ist.
-            </p>
-            <p>
-              6.3 Der Verkäufer haftet nur für Schäden, die vorsätzlich oder grob fahrlässig verursacht wurden. Eine
-              Haftung für einfache Fahrlässigkeit ist ausgeschlossen, außer bei Schäden aus der Verletzung des Lebens,
-              des Körpers oder der Gesundheit.
+              Es gilt die gesetzliche Mängelhaftung. Reklamationen müssen innerhalb der gesetzlichen Fristen gemeldet werden. 
+              Zusätzlich bietet R. eine freiwillige Garantie gemäß den separaten Garantiebedingungen, die ausdrücklich von der gesetzlichen Gewährleistung getrennt sind.
             </p>
 
-            <p className="text-lg font-semibold mt-4">7) Schlussbestimmungen</p>
-            <p>7.1 Es gilt ausschließlich deutsches Recht. Das UN-Kaufrecht wird ausdrücklich ausgeschlossen.</p>
+            <div className="border-t border-gray-500 my-6"></div>
+
+            {/* Widerrufsrecht */}
+            <p className="text-lg font-semibold">Widerrufsrecht</p>
             <p>
-              7.2 Gerichtsstand ist der Sitz des Verkäufers, sofern der Kunde Kaufmann ist oder seinen Wohnsitz
-              außerhalb Deutschlands hat.
+              Verbraucher haben ein gesetzliches Widerrufsrecht gemäß der Widerrufsbelehrung auf der Website. 
+              Bei konfigurierbaren oder individuellen PCs können Einschränkungen bestehen, da Maßanfertigungen unter Umständen vom Widerrufsrecht ausgeschlossen sind.
             </p>
 
-            <p className="text-lg font-semibold mt-4">Zusatzinformationen:</p>
+            <div className="border-t border-gray-500 my-6"></div>
+
+            {/* Haftung */}
+            <p className="text-lg font-semibold">Haftung</p>
             <p>
-              Garantieverfall: Die Garantie erlischt, wenn das Produkt an Dritte weiterverkauft oder unsachgemäß
-              verwendet wird. Kundensupport: Wir bieten selbstverständlich Support, behalten uns jedoch vor, Anfragen,
-              die nicht mit der bestellten Ware zusammenhängen, nicht zu berücksichtigen.
+              R. haftet nur für Schäden, die vorsätzlich oder grob fahrlässig verursacht wurden. 
+              Eine Haftung für einfache Fahrlässigkeit ist ausgeschlossen, soweit nicht Leben, Körper oder Gesundheit betroffen sind. 
+              Für Schäden durch unsachgemäße Nutzung, Eingriffe Dritter oder unzureichende Verpackung bei Rücksendungen übernimmt R. keine Haftung.
+            </p>
+
+            <div className="border-t border-gray-500 my-6"></div>
+
+            {/* Schlussbestimmungen */}
+            <p className="text-lg font-semibold">Schlussbestimmungen</p>
+            <p>
+              Es gilt deutsches Recht. Das UN-Kaufrecht wird ausgeschlossen. Gerichtsstand ist der Sitz des Verkäufers, 
+              sofern der Kunde Kaufmann ist oder seinen Wohnsitz außerhalb Deutschlands hat.
+            </p>
+            <p>
+              Die AGB gelten ab dem Zeitpunkt ihrer Veröffentlichung auf der Website und ersetzen alle vorherigen Fassungen. 
+              Änderungen der AGB werden auf der Website bekanntgegeben.
             </p>
           </div>
         </div>
+
+        {/* Footer */}
+        <Footer />
       </div>
-      <Footer />
     </>
   )
 }
 
 export default AGB
-
