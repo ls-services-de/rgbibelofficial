4f62453e45d41af4e71a8ec78520f2ad50580b9d
Liam Schneider <119621121+ls-services-de@users.noreply.github.com>
2025-11-10 17:42:43 +0100

    Modify layout to use German language and add analytics
    Updated the language attribute to German and added Google Analytics scripts.

--- app/layout.js
+++ app/layout.js
@@ -1,37 +1,44 @@
 import { Inter } from "next/font/google";
-import OverlayManager from "./components/OverlayManager"; // Overlay Manager für Launch/Maintenance/Passwort
+import OverlayManager from "./components/OverlayManager";
 import "./globals.css";
 import { ClerkProvider } from '@clerk/nextjs'
 import CookieConsent from "./components/CookieConsent";
 import { SpeedInsights } from '@vercel/speed-insights/next';
-
-
+import Script from "next/script"; // <--- Wichtig!
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Die besten Gaming-PCs mit Top Preis-Leistung | RGBibelOfficial",
   description: "RGBibelOfficial bietet leistungsstarke Gaming-PCs, maßgeschneiderte Streaming-Systeme und hochwertige Fertig-PCs mit unschlagbarem Preis-Leistungs-Verhältnis.",
-  
 };
 
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body className={inter.className}>
+      <html lang="de">
+        <body className={inter.className}>
+          <OverlayManager>
+            <CookieConsent />
+            {children}
+            <SpeedInsights />
+          </OverlayManager>
 
-        <OverlayManager>
-
-          {/* Der Inhalt der Seite */}
-          <CookieConsent />
-          {children}
-          <SpeedInsights />
-
-        </OverlayManager>
-        
-      </body>
-    </html>
+          {/* Google Analytics */}
+          <Script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-HQFJDEXLTQ"
+          />
+          <Script id="google-analytics">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-HQFJDEXLTQ');
+            `}
+          </Script>
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
